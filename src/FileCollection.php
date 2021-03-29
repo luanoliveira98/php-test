9f6d808b0cf2ee9e50e75236d24e17a9c9bd4a44
Luan Oliveira <luan.oliveira@uhuu.com>
2021-03-29 15:03:04 -0400

    Create file collection

---
+++ src/FileCollection.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Live\Collection;
+
+/**
+ * File collection
+ * 
+ * @package Live\Collection
+ */
+class FileCollection implements CollectionInterface
+{
+    /**
+     * Collection data
+     * 
+     * @var
+     */
+    protected $data;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get(string $index, $defaultValue = null)
+    {
+        if(!$this->has($index)) {
+            return $defaultValue;
+        }
+
+        return $this->data[$index];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function set(string $index, $value)
+    {
+        $this->data[$index] = $value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function has(string $index)
+    {
+        return array_key_exists($index, $this->data);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clean()
+    {
+        $this->data = [];
+    }
+}
